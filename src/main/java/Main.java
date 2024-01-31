1a05d37336e565bca2dff1f0a027ccd1b4b496a9
mmig01 <mintim01@naver.com>
2024-01-31 22:27:14 +0900

    템플릿 추가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
 public class Main {
-    public static void main(String[] args) {
+    private void show(){
         System.out.println("hihi");
+    }
+    public void solution() throws Exception{
+        show();
+    }
+    public static void main(String[] args) throws Exception{
+        new Main().solution();
     }
 }
